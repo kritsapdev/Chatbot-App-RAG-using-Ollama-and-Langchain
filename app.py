bf78c83522d2a611c0aeb9065200c882ab2e47c6
kritsapdev <kritsapdev@gmail.com>
2024-06-03 15:04:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -30,9 +30,25 @@ app = Flask(__name__)  # define app using Flask
 def test():
     return jsonify({'message': 'It works!'})'''
 
+import os
+from flask import Flask, jsonify, request
 from transformers import pipeline
 
-# Allocate a pipeline for sentiment-analysis
-classifier = pipeline('sentiment-analysis')
-classifier('We are very happy to include pipeline into the transformers repository.')
-[{'label': 'POSITIVE', 'score': 0.9978193640708923}]
+app = Flask(__name__)
+
+model_path = "./model"
+
+@app.route('/')
+def classify_review():
+    review = request.args.get('review')
+    api_key = request.args.get('api_key')
+    if review is None or api_key != "MyCustomerApiKey":
+        return jsonify(code=403, message="bad request")
+    classify = pipeline("sentiment-analysis", model=model_path, tokenizer=model_path)
+    return classify("that was great")[0]
+
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google Cloud
+    # Run, a webserver process such as Gunicorn will serve the app.
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
